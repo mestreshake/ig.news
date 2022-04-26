00ef3a4abec7742e6d3ea955c2fe3f93471b8c7d
Thiago Hartman <thiagolhartman@gmail.com>
2022-04-25 22:40:30 -0400

    fix: App version added to .env

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,9 +1,8 @@
 import Stripe from "stripe";
-import { version } from "../../package.json";
-export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
   apiVersion: "2020-08-27",
   appInfo: {
     name: "ig.news",
-    version,
+    version: process.env.REACT_APP_VERSION,
   },
 });
