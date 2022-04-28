cc6cc327a0500a2c61d8e01a49a829c3942bdb0c
Thiago Hartman <thiagolhartman@gmail.com>
2022-04-28 06:04:38 -0400

    fix: GIthub login + Storing user email correctly

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,22 +19,28 @@ export default NextAuth({
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
       try {
         await fauna.query(
           q.If(
             q.Not(
               q.Exists(
-                q.Match(q.Index("user_by_email"), q.Casefold(email))
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
               )
             ),
             q.Create(q.Collection("users"), { data: { email } }),
             q.Get(
-              q.Match(q.Index("user_by_email"), q.Casefold(email))
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
             )
           )
         );
-
         return true;
       } catch {
         return false;
