677101bf75b69239100211a73b0fb0c7309e46d0
Thiago Hartman <thiagolhartman@gmail.com>
2022-05-11 19:03:09 -0400

    feat: Header ActiveLink focus

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { Container, Content } from "./styles";
 import Image from "next/image";
 import logoImg from "../../../public/images/logo.svg";
 import { SignInButton } from "./SignInButton";
+import { ActiveLink } from "../../ActiveLink";
 
 export function Header() {
   return (
@@ -9,8 +10,12 @@ export function Header() {
       <Content>
         <Image src={logoImg} alt="ig.news" />
         <nav>
-          <a className="active">Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName="active" href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName="active" href="/posts">
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </Content>
