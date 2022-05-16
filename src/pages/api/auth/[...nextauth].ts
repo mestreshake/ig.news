e2edb1c6a26f0d572aa934631864dff18d5e21a4
Thiago Hartman <thiagolhartman@gmail.com>
2022-05-15 20:36:35 -0400

    feat: Verify Active Subscription

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,26 +19,48 @@ export default NextAuth({
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+
+        return {
+          ...session.session,
+          activeSubscription: userActiveSubscription,
+        };
+      } catch {
+        return {
+          ...session.session,
+          activeSubscription: null,
+        };
+      }
+    },
+
     async signIn({ user, account, profile }) {
       const { email } = user;
       try {
         await fauna.query(
           q.If(
-            q.Not(
-              q.Exists(
-                q.Match(
-                  q.Index("user_by_email"),
-                  q.Casefold(user.email)
-                )
-              )
-            ),
+            q.Not(q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))),
             q.Create(q.Collection("users"), { data: { email } }),
-            q.Get(
-              q.Match(
-                q.Index("user_by_email"),
-                q.Casefold(user.email)
-              )
-            )
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
           )
         );
         return true;
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -34,6 +34,16 @@ export default function Post({ post }: PostProps) {
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }: any) => {
   const session = await getSession({ req });
+
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   const { slug } = params;
   const client = createClient({ req });
 
